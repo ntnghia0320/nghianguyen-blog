ac631e3296eb9e034513599d23ba646361538913
ntnghia0320 <ntnghia0320@gmail.com>
2021-07-06 16:38:15 +0700

    init project.

---
+++ src/test/java/com/ntnghia/nghianguyenblog/NghianguyenBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntnghia.nghianguyenblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NghianguyenBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
