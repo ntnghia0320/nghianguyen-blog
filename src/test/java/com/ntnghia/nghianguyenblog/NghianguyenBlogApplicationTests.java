a675fa2e50381f6b4fc6c76a901472db12256664
ntnghia <ntnghia0320@gmail.com>
2021-07-12 13:50:19 +0700

    config application-test.properties

--- src/test/java/com/ntnghia/nghianguyenblog/NghianguyenBlogApplicationTests.java
+++ src/test/java/com/ntnghia/nghianguyenblog/NghianguyenBlogApplicationTests.java
@@ -1,13 +1,25 @@
 package com.ntnghia.nghianguyenblog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class NghianguyenBlogApplicationTests {
 
+	@Value("${spring.datasource.url}")
+	private String dataSourceUrl;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
+	@Test
+	public void test_dataSourceUrl() {
+		assertEquals(dataSourceUrl, "jdbc:mysql://localhost:3306/blog_test");
+	}
 }
